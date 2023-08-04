5444bd3fc08706d9122eab0cf22675126cc9cf4d
ArberTu474 <arber.tusha@outlook.com>
2023-08-04 09:52:36 +0200

    Github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), react()]
-});
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    react(),
+  ],
+  site: 'https://www.visitdiber.com/',
+  base: '/main',
+})
